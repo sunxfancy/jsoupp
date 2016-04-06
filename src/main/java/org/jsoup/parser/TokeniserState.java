9ce0631ae696834344d7d74bb872228bb6540ea0
sxf <sun_xfancy@163.com>
2016-04-06 10:43:54 +0800

    add all tests improve profile

--- src/main/java/org/jsoup/parser/TokeniserState.java
+++ src/main/java/org/jsoup/parser/TokeniserState.java
@@ -239,13 +239,15 @@ enum TokeniserState {
             if (r.matches('/')) {
                 t.createTempBuffer();
                 t.advanceTransition(RCDATAEndTagOpen);
-            } else if (r.matchesLetter() && t.appropriateEndTagName() != null && !r.containsIgnoreCase("</" + t.appropriateEndTagName() + ">")) {
-                // diverge from spec: got a start tag, but there's no appropriate end tag (</title>), so rather than
-                // consuming to EOF; break out here
-                t.tagPending = t.createTagPending(false).name(t.appropriateEndTagName());
-                t.emitTagPending();
-                r.unconsume(); // undo "<"
-                t.transition(Data);
+
+                // sxf fix for profile
+//            } else if (r.matchesLetter() && t.appropriateEndTagName() != null && !r.containsIgnoreCase("</" + t.appropriateEndTagName() + ">")) {
+//                // diverge from spec: got a start tag, but there's no appropriate end tag (</title>), so rather than
+//                // consuming to EOF; break out here
+//                t.tagPending = t.createTagPending(false).name(t.appropriateEndTagName());
+//                t.emitTagPending();
+//                r.unconsume(); // undo "<"
+//                t.transition(Data);
             } else {
                 t.emit("<");
                 t.transition(Rcdata);
---
+++ src/test/java/org/hpar/AllTest.java
@@ -0,0 +1,62 @@
+package org.hpar;
+
+import junit.framework.TestCase;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * for all tests
+ * Created by sxf on 4/6/16.
+ */
+public class AllTest extends TestCase {
+
+    int k = 0;
+    int all = 0;
+    public void testAll() {
+        k = 0;
+        loadAllFiles("src/test/extern/websites");
+        System.out.println("正确率： " + k*100/all + "%");
+    }
+
+    public void loadAllFiles(String filePath) {
+        File f = new File(filePath);
+        File[] files = f.listFiles(); // 得到f文件夹下面的所有文件。
+        assert files != null;
+        all = files.length;
+        for(File file : files) {
+            System.out.println(file.getAbsolutePath());
+            try {
+                loadOneFile(file.getAbsolutePath());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void loadOneFile(String path) throws IOException {
+        String data = App.readFile(path);
+
+        long begin = System.nanoTime();
+        Document dd = Parser.parse(data, "");
+        long end = System.nanoTime();
+        System.out.println("normal time: " + (end - begin) / 1000000 + "ms");
+
+        Document d = null;
+        try {
+            ParallelParser pp = new ParallelParser(data, 4);
+            d = pp.parse();
+        } catch (Exception e1) {
+            e1.printStackTrace();
+            throw e1;
+        }
+        assertNotNull(d);
+        System.out.println(dd.tag());
+        System.out.println(d.tag());
+        assertTrue(d.getClass() == dd.getClass());
+        k++;
+//        assertTrue(dd.equals(d));
+    }
+}
